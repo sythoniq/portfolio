5c36a811bc7018610d86a00ee35fa80df343779b
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-04-19 14:07:01 +0300

    getting close to basics end

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,10 +5,10 @@ export default function Project({title, description, liveurl, githuburl}) {
       <div className="project-description">
         <h4>{title}</h4>
         <p>{description}</p>
-        <div className="project-links">
-          <p><a href={liveurl}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg></a></p>
-          <p><a href={githuburl}><img src="/svgs/github.svg"/></a></p>
-        </div>
+      </div>
+      <div className="project-links">
+        <p><a href={githuburl}><img src="/svgs/github.svg"/></a></p> 
+        <p><a href={liveurl}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg></a></p>
       </div>
     </div>
   )
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -8,40 +8,40 @@ import Project from '../components/Project.jsx'
 import Card from '../components/Card.jsx'
 
 export default function Root() {
-  return (
-    <>
-      <main className="portfolio">
-        <Nav />
-        <section className="hero-section" id="home">
-          <h3>Built On Curiosity, Refined By Doing</h3>
-          <p>Hi I am Abdikadir. A fullstack engineer based in Kenya, I
-            specialize in building web application. But still eager to learn.</p>
-        </section>
-        <section className="projects-section" id="projects">
-          {projects.map((project) => {
-            return (
-              <Project key={project.id} title={project.title} description={project.description} liveurl={project.liveurl} githuburl={project.githuburl} />
-            )
-            })}
-        </section>
-        <section className="about-section" id="about">
-          <h2>Current Technologies</h2>
-          <p>I am currently well versed with the technologies below and I can use them to build fast and effective web applications in a timely manner</p>
-          <div className="tech-list">
-            {skills.map((skill) => {
-              return (
-                <Card key={skill.id} name={skill.name} imgUrl={skill.imgUrl} />
-              ) 
-            })}
-          </div>
-          <div className="contact-list">
-            <p>Interested in working with me? Feel free to reach out.</p>  
-            <a href="https://github.com/sythoniq"><img src="/svgs/github.svg" /></a>
-            <a href="https://linkedin.com"><img src="/svgs/linkedin.svg" /></a>
-            <a href="https://x.com"><img src="/svgs/x.svg"/></a>
-          </div>
-        </section>
-      </main>
-    </>
-  )
+return (
+<main className="portfolio">
+<Nav />
+<section className="hero-section" id="home">
+<h3>Built On Curiosity, Refined By Doing</h3>
+<p>Hi I am Abdikadir. A fullstack engineer based in Kenya, I
+specialize in building web application. But still eager to learn.</p>
+</section>
+<section className="projects-section">
+<h1 style={{color: "white"}}>Projects</h1>
+<div className="projects-list">
+{projects.map((project) => {
+return (
+<Project key={project.id} title={project.title} description={project.description} liveurl={project.liveurl} githuburl={project.githuburl} />
+)
+})}
+</div>
+</section>
+<section className="about-section" id="about">
+<h2>Technologies</h2>
+<div className="tech-list">
+{skills.map((skill) => {
+return (
+<Card key={skill.id} name={skill.name} imgUrl={skill.imgUrl} />
+) 
+})}
+</div>
+<div className="contact-list">
+<p>Interested in working with me? Feel free to reach out.</p>  
+<a href="https://github.com/sythoniq"><img src="/svgs/github.svg" /></a>
+<a href="https://linkedin.com"><img src="/svgs/linkedin.svg" /></a>
+<a href="https://x.com"><img src="/svgs/x.svg"/></a>
+</div>
+</section>
+</main>
+)
 }
